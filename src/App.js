b928e25c7f7d8ecfb9776e963d4c9bea6fe25714
DONGWON KIM <lunacircle4@likelion.org>
2019-09-10 15:42:40 +0900

    feat: header prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="body">
+          <p>T U N A S T R E A M</p>
+          <ul className="menu">
+            <li>about me</li>
+            <li>my skils</li>
+            <li>my works</li>
+          </ul>
+          <button></button>
+        </div>
+        
+        <ul className="dropdown">
+          <li>about me</li>
+          <li>my skils</li>
+          <li>my works</li>
+        </ul>
       </header>
     </div>
   );
